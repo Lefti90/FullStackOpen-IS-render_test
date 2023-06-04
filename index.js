75505fbbbaac2cc3c16f23fcf81f9d91b2b743c1
Lefti90 <ilkka.saekkinen@gmail.com>
2023-06-04 21:19:39 +0300

    Port to case sensitive?

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ app.get('/api/persons/:id', (request, response) => {
   })
   
   //SERVER
-  const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  const port = process.env.PORT || 3001
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
   })
