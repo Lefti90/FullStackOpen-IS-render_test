94ade7d2959e02351e27626cab7026c1d556f4af
Lefti90 <ilkka.saekkinen@gmail.com>
2023-06-05 00:02:01 +0300

    index and mongo.js combined

--- index.js
+++ index.js
@@ -1,105 +1,101 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 const cors = require('cors')
 
+const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
-let persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-  },
-  {
-    "id": 5,
-    "name": "asd",
-    "number": "4321"
-  },
-  {
-    "id": 6,
-    "name": "asdasd",
-    "number": "123123"
-  },
-  {
-    "id": 7,
-    "name": "qwe",
-    "number": "123"
-  }
-]
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:', error)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
 
 //POST
-const generateId = () => {
-    const maxId = persons.length > 0
-      ? Math.max(...persons.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
-  
-  app.post('/api/persons', (request, response) => {
-    const body = request.body
-  
-    if (!body.name) {
-      console.log(body)
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
-  
-    const person = {
-      name: body.name,
-      number: body.number,
-      id: generateId(),
-    }
-  
-    persons = persons.concat(person)
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
     console.log(body)
-    response.json(person)
+    return response.status(400).json({
+      error: 'content missing',
+    })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
   })
 
-  //GET
+  person.save()
+    .then(result => {
+      console.log(`Added ${body.name} to the phonebook`)
+      response.json(result)
+    })
+    .catch(error => {
+      console.log('Error saving the person:', error)
+      response.status(500).json({ error: 'An error occurred while saving the person' })
+    })
+})
+
+//GET
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
+  const id = Number(request.params.id)
 
-  app.get('/api/persons', (req, res) => {
-    res.json(persons)
-  })
+  Person.findById(id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log('Error retrieving the person:', error)
+      response.status(500).json({ error: 'An error occurred while retrieving the person' })
+    })
+})
+
+app.get('/api/persons', (req, res) => {
+  Person.find({})
+    .then(persons => {
+      res.json(persons)
+    })
+    .catch(error => {
+      console.log('Error retrieving persons:', error)
+      res.status(500).json({ error: 'An error occurred while retrieving persons' })
+    })
+})
 
 //DELETE
-  app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
-  })
-  
-  //SERVER
-  //const PORT = process.env.PORT || 3001 <- not working on Render
-  const port = 3001
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-  })
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+
+  Person.findByIdAndRemove(id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      console.log('Error deleting the person:', error)
+      response.status(500).json({ error: 'An error occurred while deleting the person' })
+    })
+})
+
+//SERVER
+const port = process.env.PORT || 3001
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
